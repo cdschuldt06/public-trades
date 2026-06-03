f491417beb58ccde889dded0d5b529e9c3b932fb
cdschuldt06 <126021811+cdschuldt06@users.noreply.github.com>
2026-06-03 14:57:17 -0400

    add politicians API endpoints

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -19,6 +19,29 @@ app.get("/trades", async (_req, res) => {
   res.json(trades);
 });
 
+app.get("/politicians", async (_req, res) => {
+  const politicians = await prisma.politician.findMany();
+
+  res.json(politicians);
+});
+
+app.get("/politicians/:id", async (req, res) => {
+  const id = Number(req.params.id);
+
+  const politician = await prisma.politician.findUnique({
+    where: { id },
+    include: {
+      trades: {
+        include: {
+          ticker: true,
+        },
+      },
+    },
+  });
+
+  res.json(politician);
+});
+
 app.listen(port, () => {
   console.log(`PublicTrades API running on http://localhost:${port}`);
 });
