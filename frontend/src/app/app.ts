a6f8ea8d33f269b63e9c101b60f3b2c2a7b97b54
cdschuldt06 <126021811+cdschuldt06@users.noreply.github.com>
2026-06-03 15:57:17 -0400

    render trades in Angular table

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,18 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { TradesService } from './services/trades.service';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   private tradesService = inject(TradesService);
 
-  constructor() {
-    this.tradesService.getTrades().subscribe((trades) => {
-      console.log(trades);
+  trades: any[] = [];
+
+    constructor() {
+    this.tradesService.getTrades().subscribe((trades: any) => {
+      console.log("Trades loaded:", trades);
+      this.trades = trades;
     });
   }
 }
